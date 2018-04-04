b01bee693e0a3333234b13a3a99bf911476120a3
S0rrow <jae9hell@gmail.com>
2018-04-04 18:54:55 +0900

    prob3 Attendance comment added

--- src/edu/handong/csee/java/lab07/prob3/Attendance_main.java
+++ src/edu/handong/csee/java/lab07/prob3/Attendance_main.java
@@ -1,50 +1,52 @@
 package edu.handong.csee.java.lab07.prob3;
-
+/**
+ * @author s0rrow
+ * This class is a main class for printing out student's attendance using methods from Attendance class.
+ */
 import java.util.Random;
-
+// Import random for generating random numbers.
 public class Attendance_main {
+	public static void main(String[] args) {// This is a main method.
+		Attendance s_1 = new Attendance();// Declare an empty instance for first from Attendance class.
+		Attendance s_2 = new Attendance();// Declare an empty instance for second from Attendance class.
+		Attendance s_3 = new Attendance("Liel", 1, "21700726", 0);// Declare an instance for third from Attendance class about student 'Liel.'
+		Attendance s_4 = new Attendance("DoYoung", 2, "21700082", 0);// Declare an instance for fourth from Attendance class about student 'DoYoung.'
 
-	public static void main(String[] args) {
-		Attendance s_1 = new Attendance();
-		Attendance s_2 = new Attendance();
-		Attendance s_3 = new Attendance("Lucas", 1, "21833222", 0);
-		Attendance s_4 = new Attendance("Martha", 2, "21733444", 0);
+		s_1.set_name("JungYeon");// Set student name for first instance.
+		s_1.set_year(4);// Set student year for first instance.
+		s_1.set_id("21700999");// Set student id for first instance.
+		s_1.set_missed(0);// Set missed time of class for first instance.
+		s_2.set_name("ShiHyun");// Set student name for second instance.
+		s_2.set_year(2);// Set student year for second instance.
+		s_2.set_id("21700111");// Set student id for second instance.
+		s_2.set_missed(0);// Set missed time of class for second instance.
 
-		s_1.set_name("Jess");
-		s_1.set_year(4);
-		s_1.set_id("21400999");
-		s_1.set_missed(0);
-		s_2.set_name("Kent");
-		s_2.set_year(2);
-		s_2.set_id("21700111");
-		s_2.set_missed(0);
+		Random randomGenerator = new Random();// Declare random for generating random numbers.
 
-		Random randomGenerator = new Random();
-
-		s_1.set_missed(randomGenerator.nextInt(10));
-		s_2.set_missed(randomGenerator.nextInt(10));
-		s_3.set_missed(randomGenerator.nextInt(10));
-		s_4.set_missed(randomGenerator.nextInt(10));
-		if (s_1.get_missed() > 6) {
-			System.out.println("I'm sorry " + s_1.get_name() + ". You fail this course");
-			System.out.println(s_1.get_name() + " - Number of absence: " + s_1.get_missed());
+		s_1.set_missed(randomGenerator.nextInt(10));// Assign random numbers generated through randomGenerator to first instance.
+		s_2.set_missed(randomGenerator.nextInt(10));// Assign random numbers generated through randomGenerator to second instance.
+		s_3.set_missed(randomGenerator.nextInt(10));// Assign random numbers generated through randomGenerator to third instance.
+		s_4.set_missed(randomGenerator.nextInt(10));// Assign random numbers generated through randomGenerator to fourth instance.
+		if (s_1.get_missed() > 6) {// If class missed is more than 6 times,
+			System.out.println("I'm sorry " + s_1.get_name() + ". You fail this course");// Print the fail message.
+			System.out.println(s_1.get_name() + " - Number of absence: " + s_1.get_missed());// And notify the missed time.
 		} else
-			System.out.println("We'll see about the grade, " + s_1.get_name());
-		if (s_2.get_missed() > 6) {
-			System.out.println("I'm sorry " + s_2.get_name() + ". You fail this course");
-			System.out.println(s_2.get_name() + " - Number of absence: " + s_2.get_missed());
+			System.out.println("We'll see about the grade, " + s_1.get_name());// If not, print out to check the grade.
+		if (s_2.get_missed() > 6) {// If class missed is more than 6 times,
+			System.out.println("I'm sorry " + s_2.get_name() + ". You fail this course");// Print the fail message.
+			System.out.println(s_2.get_name() + " - Number of absence: " + s_2.get_missed());// And notify the missed time.
 		} else
-			System.out.println("We'll see about the grade, " + s_2.get_name());
-		if (s_3.get_missed() > 6) {
-			System.out.println("I'm sorry " + s_3.get_name() + ". You fail this course");
-			System.out.println(s_3.get_name() + " - Number of absence: " + s_3.get_missed());
+			System.out.println("We'll see about the grade, " + s_2.get_name());// If not, print out to check the grade.
+		if (s_3.get_missed() > 6) {// If class missed is more than 6 times,
+			System.out.println("I'm sorry " + s_3.get_name() + ". You fail this course");// Print the fail message.
+			System.out.println(s_3.get_name() + " - Number of absence: " + s_3.get_missed());// And notify the missed time.
 		} else
-			System.out.println("We'll see about the grade, " + s_3.get_name());
-		if (s_4.get_missed() > 6) {
-			System.out.println("I'm sorry " + s_1.get_name() + ". You fail this course");
-			System.out.println(s_4.get_name() + " - Number of absence: " + s_4.get_missed());
+			System.out.println("We'll see about the grade, " + s_3.get_name());// If not, print out to check the grade.
+		if (s_4.get_missed() > 6) {// If class missed is more than 6 times,
+			System.out.println("I'm sorry " + s_4.get_name() + ". You fail this course");// Print the fail message.
+			System.out.println(s_4.get_name() + " - Number of absence: " + s_4.get_missed());// And notify the missed time.
 		} else
-			System.out.println("We'll see about the grade, " + s_4.get_name());
+			System.out.println("We'll see about the grade, " + s_4.get_name());// If not, print out to check the grade.
 
 	}
 
